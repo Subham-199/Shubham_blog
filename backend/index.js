622e13a91f7a8bbc9843b6aee87bf89cf3adfc81
Anshuraj09 <anshuraj2771@gmail.com>
2024-09-23 20:16:16 +0530

    updated url

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-  origin: ["http://localhost:5173","https://rajblog-app.vercel.app/login"],
+  origin: process.env.FRONTEND_URL,
   methods:["GET","POST","PUT","DELETE"],
   credentials:true,
 }));
