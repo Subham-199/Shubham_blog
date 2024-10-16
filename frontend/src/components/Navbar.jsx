37b4925b237c2775b108c6cc3592315f9f708550
Anshuraj09 <anshuraj2771@gmail.com>
2024-10-16 20:36:57 +0530

    updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -34,7 +34,7 @@ function Navbar() {
       <nav className="shadow-lg px-4 py-2">
         <div className="flex items-center justify-between container mx-auto">
           <div className="font-semibold text-xl">
-            Raj<span className="text-blue-500">Blog</span>
+            Anshu<span className="text-blue-500">Blog</span>
           </div>
           {/* Desktop */}
           <div className="mx-6">
